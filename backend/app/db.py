72338e8035da6eec1efe90540512bc8df5007901
nur.junaina <junainanur@gmail.com>
2026-03-28 13:05:33 +0500

    db.py pylance bug

--- backend/app/db.py
+++ backend/app/db.py
@@ -6,7 +6,7 @@ from sqlalchemy.orm import DeclarativeBase, Session, sessionmaker
 
 
 class Settings(BaseSettings):
-    DATABASE_URL: str
+    DATABASE_URL: str = ""
 
     model_config = SettingsConfigDict(
         env_file=".env",
